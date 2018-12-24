ba4cd13cc0d47b8ffdf4d22a1b40d4f83d168f58
lifeifei <1032188134@qq.com>
2018-12-24 16:34:44 +0800

    创建

---
+++ java/src/main/java/com/carty/CartyApplication.java
@@ -0,0 +1,14 @@
+package com.carty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartyApplication.class, args);
+    }
+
+}
+
